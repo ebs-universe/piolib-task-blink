b3f13eae680cc228348479e02364483c0ecf821d
Chintalagiri Shashank <shashank@chintal.in>
2024-11-08 20:50:24 +0530

    Refactor for multiple blink algs

--- blink.c
+++ blink.c
@@ -4,20 +4,61 @@
 #include <hal/uc.h>
 
 
-cron_job_t blink_job;
-tm_system_t blink_start;
-tm_sdelta_t blink_period = 1000;
+blink_state_t blink_state = {0};
 
 
-void blink(void) {
+void blink_off(void) {
+    #if BLINK_POLARITY
+    gpio_set_output_low(BLINK_GPIO);  
+    #else
+    gpio_set_output_high(BLINK_GPIO);  
+    #endif
+}
+
+void blink_on(void) {
+    #if BLINK_POLARITY
+    gpio_set_output_high(BLINK_GPIO);  
+    #else
+    gpio_set_output_low(BLINK_GPIO);
+    #endif
+}
+
+void blink_toggle(void) {
     gpio_set_output_toggle(BLINK_GPIO);  
 }
 
-void set_blink_period(uint16_t period){
-    blink_period = period;
+void _blink_stop() {
+    blink_state.period = 0;
+    if (blink_state.mode){
+        blink_state.mode = BLINK_OFF;
+    }
+    if (blink_state.job.active) {
+        tm_cron_cancel_job(&blink_state.job);
+    }
+    blink_off();
+}
+
+void _blink_make_periodic() {
+    blink_state.mode = BLINK_PERIODIC;
+    if (blink_state.job.active) {
+        tm_cron_cancel_job(&blink_state.job);
+    }
+    tm_system_t start;
+    tm_current_time(&start);
+    tm_cron_create_job_abs(&blink_state.job, &blink_toggle, &start, &blink_state.period);
+}
+
+void set_blink_period(uint16_t period){    
+    if (!period){
+        _blink_stop();
+        return;
+    }
+    blink_state.period = period;
+    if (blink_state.mode != BLINK_PERIODIC){
+        _blink_make_periodic();
+    }
 }
 
 void start_blink_task(void) {
-    tm_current_time(&blink_start);
-    tm_cron_create_job_abs(&blink_job, &blink, &blink_start, &blink_period);
+    set_blink_period(500);
 }
--- blink.h
+++ blink.h
@@ -3,6 +3,19 @@
 #include <time/cron.h>
 #include <iomap.h>
 
+typedef enum {
+    BLINK_OFF,
+    BLINK_PERIODIC,
+    // BLINK_MORSE // Not Implemented
+} BLINK_MODE_t;
+
+typedef struct BLINK_STATE_t { 
+    BLINK_MODE_t mode;
+    cron_job_t   job;
+    tm_sdelta_t  period;
+} blink_state_t;
+
+extern blink_state_t blink_state;
 
 void start_blink_task(void);
 void set_blink_period(uint16_t period);
